e9a5a57d8e148a8f9005050ce6cc0576eb5465cb
Harry <chanharryhk@gmail.com>
2019-03-31 21:41:38 -0400

    Time to grind again

--- In Progress/non-decreasing-array.py
+++ In Progress/non-decreasing-array.py
@@ -1,18 +1,7 @@
 class Solution:
     def checkPossibility(self, nums):
-        biggerCount = 0
-        max = 0
-        if len(nums) == 1:
-            return True
-        for i in range(0, len(nums) - 1):
-            if nums[i+1] < nums[i]:
-                biggerCount += 1
-        print(biggerCount)
-        if biggerCount == 1 or biggerCount == 0:
-            return True
-        else:
-            return False
+
 
 test = [3,4,2,3]
 x = Solution()
-print(x.checkPossibility(test))
+x.checkPossibility(test)
---
+++ In Progress/range-sum-of-BST.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rangeSumBST(self, root, L, R):
+
+
+
+dictlist = [dict() for x in range(5)]
+
+print(dictlistI)
