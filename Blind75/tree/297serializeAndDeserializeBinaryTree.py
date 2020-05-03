441f656165f4be4bfd8db79799b0997d5b8725cd
Harry <chanharryhk@gmail.com>
2020-05-03 15:17:56 -0400

    Anotha one

---
+++ Blind75/tree/297serializeAndDeserializeBinaryTree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if root:
+            return [root.val, self.serialize(root.left), self.serialize(root.right)]
+        else:
+            return 'x'
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data[0] == 'x':
+            return None
+        
+        ans = TreeNode(data[0])
+        ans.left = self.deserialize(data[1])
+        ans.right = self.deserialize(data[2])
+        return ans
+ 
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
